f0048cc812d61c8e96cb5d335ea999440cdd6040
@e53e04ac <e53e04ac@gmail.com>
2023-02-04 18:52:42 +0900

    0

---
+++ index.d.ts
@@ -0,0 +1,5 @@
+/*!
+    @e53e04ac/ipp5-static/index.d.ts
+    Copyright (C) @e53e04ac
+    MIT License
+*/
---
+++ index.js
@@ -0,0 +1,9 @@
+/*!
+    @e53e04ac/ipp5-static/index.js
+    Copyright (C) @e53e04ac
+    MIT License
+*/
+
+(async function main() {
+
+})();
